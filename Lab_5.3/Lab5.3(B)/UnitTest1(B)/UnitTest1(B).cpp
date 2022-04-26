504790e5be140b1f3f77db95caf5751d46ec4800
Jonson <91306892+AndrS19@users.noreply.github.com>
2022-04-26 17:48:56 +0300

    Add files via upload

---
+++ Lab_5.3/Lab5.3(B)/UnitTest1(B)/UnitTest1(B).cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.3(B)/Lab5.3(B).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1B
+{
+	TEST_CLASS(UnitTest1B)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char newfile[15] = "newfile.bat";
+			int t = BinSearch(newfile, "One", 4, KOMP_NAYK);
+			Assert::AreEqual(t, -1);
+		}
+	};
+}

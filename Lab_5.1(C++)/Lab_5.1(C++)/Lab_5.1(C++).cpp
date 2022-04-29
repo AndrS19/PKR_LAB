c6bd685874acaad3b410c07194cecbc1fdc25a9c
Jonson <91306892+AndrS19@users.noreply.github.com>
2022-04-29 10:30:40 +0300

    Add files via upload

---
+++ Lab_5.1(C++)/Lab_5.1(C++)/Lab_5.1(C++).cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <fstream>
+#include <Windows.h>              // забезпечення відображення кирилиці
+
+using namespace std;
+
+void CreateBIN(const char* filename);
+void PrintBIN(const char* filename);
+int ProcessBIN(const char* filename, const char* newfile);
+
+int main()
+{                             // забезпечення відображення кирилиці:
+	SetConsoleCP(1251);       // встановлення сторінки win-cp1251 в потік вводу
+	SetConsoleOutputCP(1251); // встановлення сторінки win-cp1251 в потік виводу
+
+	CreateBIN("file.dat");
+	PrintBIN("file.dat");
+	ProcessBIN("file.dat", "newfile.dat");
+	PrintBIN("newfile.dat");
+	return 0;
+}
+
+void CreateBIN(const char* filename)
+{
+	char ch;
+	ofstream f(filename, ios::binary);
+
+	int x;
+	do {
+		cout << "Введіть числа: ";
+		cin >> x;
+		cout << endl;
+		f.write((char*)&x, sizeof(x)); // записали у файл
+		cout << "Додати ще? (y/n): ";
+		cin >> ch;
+		cout << endl;
+	}
+
+	while (ch == 'y' || ch == 'Y');
+	cout << endl;
+	f.close();
+}
+
+void PrintBIN(const char* filename)
+{
+	cout << endl << "\nfile \"" << filename << "\":\n";
+	ifstream f(filename, ios::binary);
+
+	int x;
+	while (f.read((char*)&x, sizeof(x)))
+		cout << x << endl;
+	cout << endl;
+}
+
+int ProcessBIN(const char* filename, const char* newfile)
+{
+	ifstream f(filename, ios::binary);
+	ofstream g(newfile, ios::binary);
+
+	int x;
+	int y = 0;
+	while (f.read((char*)&x, sizeof(x)))
+	{
+		if (x < 0)
+		{
+			y += x;
+			g.write((char*)&y, sizeof(y));
+		}
+	}
+	return y;
+}
---
+++ Lab_5.1(C++)/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_5.1(C++)/Lab_5.1(C++).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			char filename[15] = "file.dat";
+			char newfile[15] = "newfile.dat";
+			ProcessBIN(filename, newfile);
+		}
+	};
+}

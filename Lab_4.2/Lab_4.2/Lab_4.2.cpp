224e08eee5fdbcb8ad8fe5c7edada44244757731
Jonson <91306892+AndrS19@users.noreply.github.com>
2022-05-13 15:34:36 +0300

    Add files via upload

---
+++ Lab_4.2/Lab_4.2/Lab_4.2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int ProcessTXT(char* fname){
+
+	string shortest, word;
+	ifstream fin(fname); // відкрили файл для зчитування
+	fin >> word; 
+	shortest = word;
+	while (getline(fin, word)) // поки можна прочитати рядок
+	{
+		if (word.size() < shortest.size()) {
+			shortest = word;
+		}
+	}
+	cout << "The shortest word: " << shortest << endl;
+	return shortest.size();
+}
+
+int main()
+{
+	char fname[10] = "t.txt"; // ім'я файлу
+	cout << "Length = " << ProcessTXT(fname) << endl;
+	cin.get();
+	return 0;
+}
---
+++ Lab_4.2/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_4.2/Lab_4.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char fname[10] = "t.txt";
+			ProcessTXT(fname);
+		}
+	};
+}

94f60759e7b710a49756eb48b705dc57d4bcd59d
Jonson <91306892+AndrS19@users.noreply.github.com>
2022-05-19 22:18:10 +0300

    Fix code and add unit test

--- Lab_6.1/Lab_6.1/Lab_6.1.cpp
+++ Lab_6.1/Lab_6.1/Lab_6.1.cpp
@@ -11,26 +11,24 @@ int main()
 {
     Elem* p;
 
-    p = new Elem; // 1
-    p->info = 1; // 2
-    p->link1 = new Elem; // 3
-    p->link1->info = 2; // 4
+    p = new Elem;                       // 1
+    p->info = 1;                        // 2
+    p->link1 = new Elem;                // 3
+    p->link1->info = 2;                 // 4
+    p->link1->link2 = p;                // 5
+    p->link1->link1 = new Elem;         // 6
+    p->link1->link1->info = 3;          // 7
+    p->link1->link1->link2 = NULL;      // 8
+    p->link2 = new Elem;                // 9
+    p->link2->info = 4;                 // 10
+    p->link2->link1 = p;                // 11
+    p->link2->link2 = p->link1->link1;  // 12
+    p->link1->link1->link1 = p;         // 13
 
-    p->link1->link2 = p->link2; // 5
-    p->link1->link2 = new Elem; // 6
-    p->link1->link2->info = 3; // 7
-    p->link1->link2->link2 = NULL; // 8
-    p->link2 = new Elem; // 9
-    p->link2->info = 4; // 10
-    p->link2->link1 = p; // 11
-
-    p->link2->link2 = p->link1->link2->link2; // 12
-    p->link1->link2->link1 = p->link2; // 13
-
-    delete p->link2; // 14
-    delete p->link1->link2; // 15
-    delete p->link1; // 16
-    delete p; // 17
+    delete p->link2;                    // 14
+    delete p->link1->link1;             // 15
+    delete p->link1;                    // 16
+    delete p;                           // 17
 
     return 0;
 }
--- Lab_6.1/UnitTest1/UnitTest1.cpp
+++ Lab_6.1/UnitTest1/UnitTest1.cpp
@@ -12,7 +12,10 @@ namespace UnitTest1
 		
 		TEST_METHOD(TestMethod1)
 		{
-
+			Elem* p = new Elem;
+			p->info = 4;
+			Assert::AreEqual(4, p->info);
+			delete p;
 		}
 	};
 }

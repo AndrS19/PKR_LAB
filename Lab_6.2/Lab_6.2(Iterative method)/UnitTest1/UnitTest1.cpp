e46f5cff152e85419c3e0fdceb44c6ef5d190663
Jonson <91306892+AndrS19@users.noreply.github.com>
2022-05-20 17:35:14 +0300

    Fix Unit Test

--- Lab_6.2/Lab_6.2(Iterative method)/UnitTest1/UnitTest1.cpp
+++ Lab_6.2/Lab_6.2(Iterative method)/UnitTest1/UnitTest1.cpp
@@ -12,16 +12,19 @@ namespace UnitTest1
 		
 		TEST_METHOD(TestMethod1)
 		{
-			L* first = NULL;
-			L* last = NULL;
-			int N = 5;
-			int Min = -5;
-			int Max = 5;
-			for (int i = 0; i < N; i++)
-			{
-				int x = Min + rand() % (Max - Min + 1);
-				Enqueue(first, last, x);
-			}
+			L* tmp;
+			tmp = new L;
+			tmp->info = 10;
+			tmp->link = new L;
+			tmp->link->info = 20;
+			tmp->link->link = new L;
+			tmp->link->link->info = 30;
+
+			L* first = tmp->link->link;
+
+			Enqueue(tmp, first, 50);
+
+			Assert::AreEqual(tmp->link->link->link->info, 50);
 		}
 	};
 }

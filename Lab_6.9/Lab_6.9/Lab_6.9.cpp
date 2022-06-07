b98ef4e457b29ac1d88c6ad8706015207326a6e4
Jonson <91306892+AndrS19@users.noreply.github.com>
2022-06-07 13:48:11 +0300

    Add files via upload

---
+++ Lab_6.9/Lab_6.9/Lab_6.9.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+
+using namespace std;
+
+typedef int Info;
+struct Node
+{
+	Node* left,
+		* right;
+	Info info;
+	char ch;
+};
+
+Node* CreateTree(int nodeCount);
+void PrintTree(Node* root, int level);
+Info InfixOrder(Node* root, Info& min, char& ch);
+
+int main()
+{
+	char ch;
+	int N, min = INT_MAX;
+	cout << "Enter nodes count: "; cin >> N;
+	Node* root = CreateTree(N);
+	cout << endl;
+	PrintTree(root, 0);
+	cout << "Infix search: ";
+	InfixOrder(root, min, ch);
+	cout << endl << min << ch << endl;
+	return 0;
+}
+
+Node* CreateTree(int nodeCount)
+{
+	if (nodeCount == 0)
+		return NULL;
+	else
+	{
+		Node* newNode = new Node;
+
+		cout << " Enter node value: ";
+		cin >> newNode->info;
+		cout << " Enter node symbol: ";
+		cin >> newNode->ch;
+
+		int leftCount = nodeCount / 2;
+		int rightCount = nodeCount - leftCount - 1;
+		newNode->left = CreateTree(leftCount);
+		newNode->right = CreateTree(rightCount);
+		return newNode;
+	}
+}
+
+void PrintTree(Node* root, int level)
+{
+	if (root != NULL)
+	{
+		PrintTree(root->right, level + 1);
+		for (int i = 1; i <= level; i++)
+			cout << "   ";
+		cout << root->info;
+		cout << root->ch << endl;
+		PrintTree(root->left, level + 1);
+	}
+}
+
+Info InfixOrder(Node* root, Info& min, char& ch)
+{
+	if (root != NULL)
+	{
+		InfixOrder(root->left, min, ch);
+		cout << root->info << "";
+		cout << root->ch << "   ";
+		InfixOrder(root->right, min, ch);
+		if (root->info <= min) {
+			min = root->info;
+			ch = root->ch;
+		}
+	}
+	return ch;
+}
---
+++ Lab_6.9/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_6.9/Lab_6.9.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			/*Node* root = NULL;
+			int nodeCount{};
+			Node* newNode = new Node;
+			newNode->info = 7;
+			int leftCount = nodeCount / 2;
+			newNode->left = CreateTree(nodeCount);
+			Assert::IsNotNull(root);*/
+		}
+	};
+}
